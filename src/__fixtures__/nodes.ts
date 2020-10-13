ae1296c5fd4a19d42807df0aeefb99c5d59447a9
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-10-13 14:13:26 +0200

    Add tests for wrappers

--- src/__fixtures__/nodes.ts
+++ src/__fixtures__/nodes.ts
@@ -38,3 +38,24 @@ export const EXPANDE_NODE: Network = {
   networkId: 1,
   slip44: 40
 };
+
+export const TEST_NODE: Network = {
+  name: 'Ethereum Mainnet',
+  chain: 'ETH',
+  network: 'mainnet',
+  rpc: ['http://localhost:8545'],
+  faucets: [],
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  infoURL: 'https://ethereum.org',
+  shortName: 'eth',
+  chainId: 1,
+  networkId: 1,
+  slip44: 60,
+  ens: {
+    registry: '0x314159265dd8dbb310642f98f50c066173c1259b'
+  }
+};
